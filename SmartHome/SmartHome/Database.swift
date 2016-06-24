100107158a461b4220b9da427a807f3e6855a448
Artem Kirienko <akirienko@envionsoftware.com>
2016-06-24 15:36:37 +0300

    Data writing implemented

--- SmartHome/SmartHome/Database.swift
+++ SmartHome/SmartHome/Database.swift
@@ -11,16 +11,39 @@ import Foundation
 class Database {
     static let sharedDatabase = Database()
     
+    private static let filePath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).last!.URLByAppendingPathComponent("output.csv")
+    private var fileHandle: NSFileHandle
+    
+    init() {
+        if !NSFileManager.defaultManager().fileExistsAtPath(Database.filePath.path!) {
+            NSFileManager.defaultManager().createFileAtPath(Database.filePath.path!, contents: nil, attributes: nil)
+        }
+        fileHandle = try! NSFileHandle(forWritingToURL: Database.filePath)
+    }
+    
+    deinit {
+        fileHandle.closeFile()
+    }
+    
+    private let isolationQueue = dispatch_queue_create("CSV writing queue", DISPATCH_QUEUE_SERIAL)
+    
     func setRange(range: Int, beacon: Beacon, time: NSDate) {
-        // Save entry to DB
-        print("\(time) <\(beacon.uuid):\(range)>")
+        let csv = "\(time.timeIntervalSince1970);\(beacon.uuid);\(range);\(beacon.name)\n"
+        print(csv)
+        
+        dispatch_async(isolationQueue) {
+            self.fileHandle.seekToEndOfFile()
+            self.fileHandle.writeData(csv.dataUsingEncoding(NSUTF8StringEncoding)!)
+        }
     }
     
     func dump() {
-        // Dump database for further investigtion
-        // 1. Export DB content to .csv
-        // 2. Clean DB
         print("dump")
+        
+        dispatch_async(isolationQueue) {
+            self.fileHandle.closeFile()
+            self.fileHandle = try! NSFileHandle(forWritingToURL: Database.filePath)
+        }
     }
     
     func getBeacons() -> [Beacon] {
