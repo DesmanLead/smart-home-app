c0fe31d750ea31c1e419f022ba20297c8593a5af
Artem Kirienko <artem.kirienko@gmail.com>
2017-06-25 16:25:48 +0300

    Set up for Max

--- SmartHome/SmartHome/Database.swift
+++ SmartHome/SmartHome/Database.swift
@@ -124,24 +124,25 @@ class Database
     {
         return [
             Device(identifier: "c2bc678c-ed73-45c6-8804-bcaf645f0891", name: "Kitchen Light", isEnabled: false),
+            Device(identifier: "1b0ada96-f4e0-4edc-8441-d0ed12d9ba53", name: "Kitchen Water Light", isEnabled: false),
             Device(identifier: "1b0ada96-f4e0-4edc-8441-d0ed12d9ba53", name: "Kitchen Local Light", isEnabled: false),
             Device(identifier: "0b6889f4-b87c-46c5-98c9-b8c3263cd8eb", name: "Kitchen Kettle", isEnabled: false),
             Device(identifier: "ed646245-3030-4251-8320-14e228fed987", name: "Bedroom Light", isEnabled: false),
             Device(identifier: "9660cb58-ab9f-448b-add8-ed5d76e3de66", name: "Bedroom Local Light", isEnabled: false),
-            Device(identifier: "0489ee1d-8919-42ce-a130-14aa0562f48b", name: "Bedroom TV", isEnabled: false),
-            Device(identifier: "66ca0cbc-6d48-4264-be78-8914412f27ed", name: "Bathroom Light", isEnabled: false),
             Device(identifier: "42c46854-3ddd-4896-b271-c051e4edacb8", name: "Toilet Light", isEnabled: false),
             Device(identifier: "12421fda-3bfd-4eda-8534-ca7f5ba8bf8f", name: "Hall Light", isEnabled: false),
             Device(identifier: "dcdb6cef-f9b3-461e-81b2-54ad37736ef4", name: "Kitchen Water", isEnabled: false),
-            Device(identifier: "1d145529-a34b-4b9c-94dc-df1021589cd9", name: "Bathroom Water", isEnabled: false),
-            Device(identifier: "a9a2eefe-f409-45b2-92ee-d5e5cd3d29c2", name: "Bathroom Shower", isEnabled: false)
+            Device(identifier: "4faee1a1-d3d7-48c7-a097-42bcdd87e9a9", name: "Kitchen Cooker", isEnabled: false),
+            Device(identifier: "35cf38bf-693e-412b-89ac-88a5998f66c0", name: "Computer", isEnabled: false),
+            Device(identifier: "889f3ef1-4c37-4f79-b19b-e78e512b82dd", name: "Music", isEnabled: false)
         ]
     }
     
     func getActivities() -> [Activity]
     {
         return [
-            Activity(identifier: "7c5d5f96-4667-4d35-9bf1-4cf604e2bc35", name: "Watching TV", isStarted: false),
+            Activity(identifier: "7c5d5f96-4667-4d35-9bf1-4cf604e2bc35", name: "Working with computer", isStarted: false),
+            Activity(identifier: "def2c8f2-a7ea-47a4-a58a-a5544e917023", name: "Rest with computer", isStarted: false),
             Activity(identifier: "c1678225-c5cf-4399-ad48-0703dc718a86", name: "Shower", isStarted: false),
             Activity(identifier: "3db877ff-bd74-4ddc-a3ec-939c57b364a3", name: "Washing hands", isStarted: false),
             Activity(identifier: "65865b45-765b-4038-9e92-d9a5c358fba9", name: "Breakfast", isStarted: false),
@@ -151,7 +152,8 @@ class Database
             Activity(identifier: "7e9cac8b-cca2-4396-b325-23edc21fde3a", name: "Toilet", isStarted: false),
             Activity(identifier: "4174d1ab-cbe7-44e8-a72e-4d6b04ae7eb7", name: "Entered home", isStarted: false),
             Activity(identifier: "5d98492f-f954-4419-a59f-d53d9fd44ef9", name: "Leaving home", isStarted: false),
-            Activity(identifier: "6242f8db-b666-4f50-8b64-05435d601aa7", name: "Preparing to sleep", isStarted: false)
+            Activity(identifier: "6242f8db-b666-4f50-8b64-05435d601aa7", name: "Preparing to sleep", isStarted: false),
+            Activity(identifier: "95eddaf9-69be-41bc-9eaf-6a4e524ba48e", name: "Washing dishes", isStarted: false)
         ]
     }
 }
